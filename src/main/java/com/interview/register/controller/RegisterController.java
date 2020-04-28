8dc37ef441fb88616d99d29480acbe161216918e
Allen0003 <apss1943@gmail.com>
2020-04-28 17:42:21 +0800

    start adding service

--- src/main/java/com/interview/register/controller/RegisterController.java
+++ src/main/java/com/interview/register/controller/RegisterController.java
@@ -1,21 +1,21 @@
 package com.interview.register.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
 import com.interview.register.entity.User;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegisterController {
 
-//    @GetMapping("/amazing-talker/register")
-    @PostMapping("/amazing-talker/register")
-//    @PathVariable User registerUser
-    public User findById() {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @PostMapping("/amazing-talker/register")
+    public User findById(@RequestBody User registerUser) {
+
+        System.out.println(registerUser.getName());
         System.out.println(" hello ~~ ");
 
         return null;
---
+++ src/main/java/com/interview/register/repository/RegisterService.java
@@ -0,0 +1,16 @@
+package com.interview.register.repository;
+
+import com.interview.register.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RegisterService {
+
+    private boolean checkEmail(User user) {
+
+        return true;
+
+    }
+
+
+}
